5052e7782b6e651a72f52aa7847550bed531b4b8
Ricardo Shiota Yasuda <shadow11@gmail.com>
2013-08-22 23:31:34 -0300

    Supports Rails 2 style validations; Updates README

--- lib/phones-brazil/cell_phone_validator.rb
+++ lib/phones-brazil/cell_phone_validator.rb
@@ -1,11 +1,24 @@
-module PhonesBrazil
-  class CellPhoneValidator < ActiveModel::Validator
-    def validate(record)
-      record.errors.add :base, :invalid unless phone_valid?(record.area,record.number)
-    end
+require "active_record"
 
-    def phone_valid?(area,number)
-      PhonesBrazil::CellPhone.new(area,number).valid?
+module PhonesBrazil
+  module Validator
+    class CellPhoneValidator < ActiveModel::Validator
+      def validate(record)
+        record.errors.add :base, :invalid unless phone_valid?(record.area,record.number)
+      end
+
+      def phone_valid?(area,number)
+        PhonesBrazil::CellPhone.new(area,number).valid?
+      end
+    end
+  end
+
+  module ClassMethods
+    def validates_cellphone
+      validates_with ActiveRecord::Base::CellPhoneValidator
     end
   end
 end
+
+ActiveRecord::Base.send(:include, PhonesBrazil::Validator)
+ActiveRecord::Base.send(:extend, PhonesBrazil::ClassMethods)
