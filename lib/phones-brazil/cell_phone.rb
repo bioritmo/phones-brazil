5d8d1280865816b9a0343a4bee832088f6871553
Nícolas Marçal <nih.williams@gmail.com>
2014-06-27 19:51:48 -0300

    using method area codes public

--- lib/phones-brazil/cell_phone.rb
+++ lib/phones-brazil/cell_phone.rb
@@ -10,9 +10,21 @@ module PhonesBrazil
       valid_area? && valid_number? && valid_number_size?
     end
 
+    def self.area_codes
+      (11..19).to_a +                   # SP
+      [21,22,24,27,28] +                # RJ,ES
+      (31..35).to_a + [37,38] +         # MG
+      (41..49).to_a +                   # PR, SC
+      [51] + (53..55).to_a +            # RS
+      (61..69).to_a +                   # DF, GO, MT, MS, AC, RO
+      [71] + (73..75).to_a + [77,79] +  # BA, SE
+      (81..89).to_a +                   # PE, AL, PB, RN, CE, PI
+      (91..99).to_a                     # PA, AM, RR, AP, MA
+    end
+
   private
     def valid_area?
-      area_codes.include?(@area)
+      CellPhone.area_codes.include?(@area)
     end
 
     def valid_number?
@@ -40,18 +52,6 @@ module PhonesBrazil
 
     def is_included?(range, number)
       sme_ranges[range].any? {|r| r.include?(number)}
-    end
-
-    def area_codes
-      (11..19).to_a +                   # SP
-      [21,22,24,27,28] +                # RJ,ES
-      (31..35).to_a + [37,38] +         # MG
-      (41..49).to_a +                   # PR, SC
-      [51] + (53..55).to_a +            # RS
-      (61..69).to_a +                   # DF, GO, MT, MS, AC, RO
-      [71] + (73..75).to_a + [77,79] +  # BA, SE
-      (81..89).to_a +                   # PE, AL, PB, RN, CE, PI
-      (91..99).to_a                     # PA, AM, RR, AP, MA
     end
 
     def sme_ranges
